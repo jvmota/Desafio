99b8bc18f11655d911237ab2a987a3a2fad91225
Jhon <Jhon@localhost.localdomain>
2019-07-13 20:02:30 -0300

    0.0.1

--- Desafio/src/main/java/com/example/desafio/Tarefa.java
+++ Desafio/src/main/java/com/example/desafio/Tarefa.java
@@ -1,5 +1,50 @@
 package com.example.desafio;
 
 public class Tarefa {
+	//atributos da tarefa
+	private int tarefaID;
+	private boolean concluido;
+	private String nome;
+	private String descricao;
 	
+	//construtor
+	public Tarefa(int id, String nome, String descricao) {
+		this.tarefaID = id;
+		this.concluido = false;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+	
+	//metodos get e set
+	public int getID() {
+		return this.tarefaID;
+	}
+	
+	public void setID(int id) {
+		this.tarefaID = id;
+	}
+	
+	public boolean getStatus() {
+		return this.concluido;
+	}
+	
+	public void setStatus(boolean status) {
+		this.concluido = status;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
--- Desafio/src/main/java/com/example/desafio/ui/controller/TarefasController.java
+++ Desafio/src/main/java/com/example/desafio/ui/controller/TarefasController.java
@@ -2,18 +2,28 @@ package com.example.desafio.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.desafio.Tarefa;
 
 @RestController
 @RequestMapping("tarefas") //http://localhost:8080/tarefas
 public class TarefasController {
 	
 	@GetMapping
-	public String getTarefa() {
-		return "Retorna Tarefa";
+	public Tarefa getTarefas(@RequestParam(value="page", defaultValue = "1") int page) {
+		Tarefa novaTarefa = new Tarefa(1, "nome", "descrição");
+		return novaTarefa;
+	}
+	
+	@GetMapping(path="/{tarefaID}")
+	public String getTarefa(@PathVariable String tarefaID) {
+		return "Retorna Tarefa com id = " + tarefaID;
 	}
 	
 	@PostMapping
